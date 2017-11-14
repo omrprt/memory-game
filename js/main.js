56096c490d1b08ab61ed99617ca302a8fa139877
Omar Portillo <omrprt@omars-mbp.home>
2017-11-14 01:23:32 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var CardFour = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+
+//cards
+var cards = ["queen", "queen","king","king"];
+
+//contains the cards that are in play.
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+//adding cardOne to cardInPlay
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+//logic to test if user has picked two cards and compares both cards
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")}
+	else {alert("Sorry, try again")}}
+
+	
